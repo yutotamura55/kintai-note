c0486ac17d8951bd0f1573b17a19bbbb1f1aa931
Yuto Tamura <yutotamura55@gmail.com>
2026-09-18 01:00:56 +0900

    Preserve imported D1 database settings

--- iac/main.tf
+++ iac/main.tf
@@ -5,8 +5,15 @@ resource "cloudflare_d1_database" "kintai_note" {
   name                  = var.d1_database_name
   primary_location_hint = var.d1_primary_location_hint
 
+  read_replication = {
+    mode = "disabled"
+  }
+
   # A replacement destroys all attendance records. Change deliberately, after backup.
   lifecycle {
     prevent_destroy = true
+
+    # This creation-time hint may be absent from imported state; keep the existing DB.
+    ignore_changes = [primary_location_hint]
   }
 }
